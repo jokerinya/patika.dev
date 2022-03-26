ec19a814e0e2942156291a9d6cbfdb86afd0bf6b
Jokerinya <sakacibrahim@gmail.com>
2022-03-26 20:57:01 +0200

    Node.js Homework8 has been done

--- Node.js/Homeworks/CleanBlog/index.js
+++ Node.js/Homeworks/CleanBlog/index.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public')); // Static Files
+
+// Routes Start
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  res.render('index');
 });
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+// Routes End
 
 const port = process.env.PORT || 3000;
 
